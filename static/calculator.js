49387b34d98b7dc7a258f9d2c50495a96c58ca47
Abhishek Nagekar <abhisheknagekar@gmail.com>
2016-03-25 12:41:07 +0530

    cleaned some code in calc js

--- static/calculator.js
+++ static/calculator.js
@@ -1,118 +1,64 @@
 window.onload = function() {
-
-var current,
-    screen,
-    output,
-    limit,
-    zero,
-    period,
-    operator;
-    
+    let current,
+        screen,
+        output,
+        limit,
+        zero,
+        period,
+        operator;
     screen = document.getElementById("result");
-
-var elem = document.querySelectorAll(".num");
-    
-      var len = elem.length;
-    
-      for(var i = 0; i < len; i++ ) {
-        
-        elem[i].addEventListener("click",function() {
-                  
+    let elem = document.querySelectorAll(".num");
+    let len = elem.length;
+    for(let i = 0; i < len; i++ ) {
+        elem[i].addEventListener("click", function() {
             num = this.value;
-                     
             output = screen.innerHTML +=num;
-                  
             limit = output.length;
-         
-         if(limit > 16 ) {
-        
-         alert("Sorry no more input is allowed");
-             
-       }
-       
-     },false);
-        
+            if(limit > 16 ) {
+                alert("Sorry no more input is allowed");
+            }
+        },false);
     } 
-
     document.querySelector(".zero").addEventListener("click",function() {
-        
         zero = this.value;
-        
         if(screen.innerHTML === "") {
-            
-           output = screen.innerHTML = zero;  
+            output = screen.innerHTML = zero;  
         }
-        
         else if(screen.innerHTML === output) {
-            
-         output = screen.innerHTML +=zero;
-            
+            output = screen.innerHTML +=zero;
         }
-          
     },false);
-    
     document.querySelector(".period").addEventListener("click",function() {
-        
         period = this.value;
-        
         if(screen.innerHTML === "") {
-            
-         output = screen.innerHTML = screen.innerHTML.concat("0.");
-            
-         }
-    
+            output = screen.innerHTML = screen.innerHTML.concat("0.");
+        }
         else if(screen.innerHTML === output) {
-        
-          screen.innerHTML = screen.innerHTML.concat(".");
-            
+            screen.innerHTML = screen.innerHTML.concat(".");
         }
-        
     },false);
-    
-    
     document.querySelector("#eqn-bg").addEventListener("click",function() {
-        
-      if(screen.innerHTML === output) {
-          
-        screen.innerHTML = eval(output);
-      }
-        
-      else {
+        if(screen.innerHTML === output) {
+            screen.innerHTML = eval(output);
+        }
+        else {
             screen.innerHTML = "";
-      }
-          
+        }
     },false);
-    
- document.querySelector("#delete").addEventListener("click",function() {
-        
+    document.querySelector("#delete").addEventListener("click",function() {
         screen.innerHTML = "";
-        
     },false);
-    
-   
-     var elem1 = document.querySelectorAll(".operator");
-    
-      var len1 = elem1.length;
-    
-      for(var i = 0; i < len1; i++ ) {
-        
+    let elem1 = document.querySelectorAll(".operator");
+    let len1 = elem1.length;
+    for(let i = 0; i < len1; i++ ) {
         elem1[i].addEventListener("click",function() {
-         
-        operator = this.value;
-         
-         if(screen.innerHTML === "") {
-            
-            screen.innerHTML = screen.innerHTML.concat("");
-            
-        }
-        
-        else if(output) {
-        
-            screen.innerHTML = output.concat(operator);
-            
-        }
-           
-    },false);
-          
-      }   
+            operator = this.value;
+            if(screen.innerHTML === "") {
+                screen.innerHTML = screen.innerHTML.concat("");
+            }
+            else if(output) {
+                screen.innerHTML = output.concat(operator);
+            }
+        },false);
+    }   
 }
